8d03d1e58a663c112662a07237ade3131e799e97
larix45 <kw316241@student.polsl.pl>
2026-05-18 13:11:28 +0200

    Initial commit

---
+++ src/main/java/ovh/warczek/sklep_internetowy/SklepInternetowyApplication.java
@@ -0,0 +1,13 @@
+package ovh.warczek.sklep_internetowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SklepInternetowyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SklepInternetowyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ovh/warczek/sklep_internetowy/SklepInternetowyApplicationTests.java
@@ -0,0 +1,13 @@
+package ovh.warczek.sklep_internetowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SklepInternetowyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
