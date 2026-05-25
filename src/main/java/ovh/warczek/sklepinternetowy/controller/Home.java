f8f3a4a2a4db217304ccaef8fea0a6dd4c740d24
larix45 <kw316241@student.polsl.pl>
2026-05-25 14:26:14 +0200

    LAB04

--- src/main/java/ovh/warczek/sklepinternetowy/controller/Home.java
+++ src/main/java/ovh/warczek/sklepinternetowy/controller/Home.java
@@ -1,16 +1,17 @@
 package ovh.warczek.sklepinternetowy.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import ovh.warczek.sklepinternetowy.model.Item;
 import ovh.warczek.sklepinternetowy.repository.ItemRepository;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class Home {
@@ -37,6 +38,27 @@ public class Home {
     public Item item()
     {
         return new Item("Laptop", new BigDecimal("100.00"), "https://picsum.photos/200");
+    }
+
+    @GetMapping("/add/{itemId}")
+    public String addItemToCart(@PathVariable("itemId") long id, Model model, HttpSession session)
+    {
+        @SuppressWarnings("unchecked")
+        List<Item> cart = (List<Item>)session.getAttribute("cart");
+        if(cart == null)
+        {
+            cart = new ArrayList<Item>();
+        }
+        Optional<Item> itemToAdd = repo.findById(id);
+        if(itemToAdd.isPresent())
+        {
+            Item item = itemToAdd.get();
+            cart.add(item);
+            session.setAttribute("cart", cart);
+        }
+
+        model.addAttribute("items", repo.findAll());
+        return "home.html";
     }
 
     @GetMapping("/cartView")
